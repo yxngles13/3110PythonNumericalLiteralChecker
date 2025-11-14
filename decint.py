9c5697f8e4e540839eccc4fda5b10eba24c49715
jo-seashell <jrvillar@cpp.edu>
2025-11-13 20:37:06 -0800

    Update decint.py

--- decint.py
+++ decint.py
@@ -1,7 +1,18 @@
 # checking if valid decinteger
 
-#function to check if decimal integer
+#boolean function to check if decimal integer
 #cannot use int, float, s.isdigit, regex libraries
 #can use for/while, if else, <= ‘0-9’
 
-def is_decint(s):
+def is_decint(c, state):
+
+    ##if the length of input string is 0, return False
+    if len(input) == 0:
+        return False
+    
+    ##loop through each character in the string
+    for char in input:
+        ##check if the character is less than '0' or greater than '9'
+        if char < '0' or char > '9':
+            return False
+    
