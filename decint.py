befc9b47c861039dfeea438381d03a588b0ae34b
jo-seashell <jrvillar@cpp.edu>
2025-11-13 18:43:32 -0800

    Create decint.py

---
+++ decint.py
@@ -0,0 +1,7 @@
+# checking if valid decinteger
+
+#function to check if decimal integer
+#cannot use int, float, s.isdigit, regex libraries
+#can use for/while, if else, <= ‘0-9’
+
+def is_decint(s):
