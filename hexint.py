0c4f55b3e58306283ab3a7a5ecdd03719248ecf0
jo-seashell <jrvillar@cpp.edu>
2025-11-15 02:54:35 -0800

    update hexint.py
    apply nfa logic to hexinteger function

--- hexint.py
+++ hexint.py
@@ -9,5 +9,34 @@ def is_hexint(s):
     ##if the length of input string is 0, return False
     if len(s) == 0:
         return False
-    
-    
+    state = 'q1'
+
+    ##loop through each character in the string
+    for char in s:
+        if state == 'q1':
+            ##check if character is a digit 0-9, a-f, A-F
+            if ('0' <= char <= '9') or ('a' <= char <= 'f') or ('A' <= char <= 'F'):
+                state = 'q2'
+            else:
+                return False
+        elif state == 'q2':
+            ##check if character is a digit 0-9, a-f, A-F
+            if ('0' <= char <= '9') or ('a' <= char <= 'f') or ('A' <= char <= 'F'):
+                state = 'q2'
+            ## check if character is underscore, if so transition to q3
+            elif char =='_':
+                state = 'q3'
+            else:
+                return False
+            ## after underscore, must have digit 0-9, a-f, A-F, transition back to q2
+        elif state == 'q3':
+            if ('0' <= char <= '9') or ('a' <= char <= 'f') or ('A' <= char <= 'F'):
+                state = 'q2'
+            else:
+                return False
+            
+    ##if we end in state q2, return True
+    if state == 'q2':
+        return True
+    else:
+        return False
