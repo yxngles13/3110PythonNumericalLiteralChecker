be7da200002651853dfdc5ce6186d334faff1ad2
jo-seashell <jrvillar@cpp.edu>
2025-11-15 02:10:51 -0800

    Update hexint.py

--- hexint.py
+++ hexint.py
@@ -1,1 +1,13 @@
 #check if valid hexinteger using nfa approach
+
+#boolean function to check if hex integer
+#cannot use int, float, s.isdigit, regex libraries
+#can use for/while, if else, <= ‘0-9’
+
+def is_hexint(s):
+
+    ##if the length of input string is 0, return False
+    if len(s) == 0:
+        return False
+    
+    
