d7524f1dca3e27b323980675e70f5d6f8aeba5df
jo-seashell <jrvillar@cpp.edu>
2025-11-15 01:53:36 -0800

    update decint.py
    updated decint with state-based validation

--- decint.py
+++ decint.py
@@ -4,15 +4,39 @@
 #cannot use int, float, s.isdigit, regex libraries
 #can use for/while, if else, <= ‘0-9’
 
-def is_decint(c, state):
+def is_decint(s):
 
     ##if the length of input string is 0, return False
-    if len(input) == 0:
+    if len(s) == 0:
         return False
     
+    state = 'q1'
+
     ##loop through each character in the string
-    for char in input:
-        ##check if the character is less than '0' or greater than '9'
-        if char < '0' or char > '9':
-            return False
+    for char in s:
+        if state == 'q1':
+            ##check if character is a digit 1-9
+            if '1' <= char <= '9':
+                state = 'q2'
+            else:
+                return False
+        elif state == 'q2':
+            ##check if character is a digit
+            if '0' <= char <= '9':
+                state = 'q2'
+            elif char =='_':
+                state = 'q3'
+            else:
+                return False
+        elif state == 'q3':
+            if '0' <= char <= '9':
+                state = 'q2'
+            else:
+                return False
+            
+    ##if we end in state q2, return True
+    if state == 'q2':
+        return True
+    else:
+        return False
     
