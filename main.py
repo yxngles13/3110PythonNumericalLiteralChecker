c5e7cfb27abef7feda600e70695693e75e0c2ed6
yxngles13 <yongthulawong@gmail.com>
2025-11-16 13:53:57 -0800

    created a test file

--- main.py
+++ main.py
@@ -1,31 +1,38 @@
-#include input file and output file
-#merge dectint, octint, hexint into this file
-
 from decint import is_decint
 from hexint import is_hexint
 from octint import is_octint
 from floating import is_floatingpoint
 
-#main function
 
 def main():
     while True:
-        print("---------Welcome to Python Numerical Literal Checker:---------")
+        print("---------Welcome to Python Numerical Literal Checker---------")
         print("1. Enter String to Check")
         print("2. Check Test File")
         print("Enter q to quit")
-        choice = input("Enter choice: ")
+        choice = input("Enter choice: ").strip()
 
-        if choice == 'q':
+        if choice.lower() == 'q':
             print("\nEnd of program.")
             break
+
         elif choice == '1':
-            input_str = input("Enter string to check:")
+            input_str = input("Enter string to check: ").strip()
             if is_decint(input_str) or is_octint(input_str) or is_hexint(input_str) or is_floatingpoint(input_str):
-                print("\nAccept")
+                print("\nAccept\n")
             else:
-                print("\nReject")            
-        
+                print("\nReject\n")
+
+        elif choice == '2':
+            input_file = "in.txt"
+            output_file = "in_ans.txt"
+
+            
+
+
+        else:
+            print("\nInvalid choice. Please try again.\n")
+
 
 if __name__ == "__main__":
     main()
