5991a0b2b387e3810cb108ff80a612ceea95fd91
jo-seashell <jrvillar@cpp.edu>
2025-11-15 19:15:11 -0800

    Fixed Octint.py
    Added beginning states for 0o and 0O

--- octint.py
+++ octint.py
@@ -13,30 +13,47 @@ def is_octint(s):
 
     ##loop through each character in the string
     for char in s:
+
         if state == 'q1':
-            ##check if character is a digit 0-7
-            if '0' <= char <= '7':
-                state = 'q2'
-            else:
-                return False
-        elif state == 'q2':
-            ##check if character is a digit 0-7
-            if '0' <= char <= '7':
-                state = 'q2'
-            ## check if character is underscore, if so transition to q3
-            elif char =='_':
-                state = 'q3'
-            else:
-                return False
-            ## after underscore, must have digit 0-7, transition back to q2
-        elif state == 'q3':
-            if '0' <= char <= '7':
+            #first state must be 0
+            if char == '0':
                 state = 'q2'
             else:
                 return False
             
-    ##if we end in state q2, return True
-    if state == 'q2':
+        elif state == 'q2':
+            #second state must be 'o' or 'O'
+            if char == 'o' or char == 'O':
+                state = 'q3'
+            else:
+                return False
+            
+        elif state == 'q3':
+            ##third state: check if character is a digit 0-7
+            if '0' <= char <= '7':
+                state = 'q4'
+            else:
+                return False
+            
+        elif state == 'q4':
+            ##fourth state: check if character is a digit 0-7
+            if '0' <= char <= '7':
+                state = 'q4'
+            ##check if character is underscore, if so transition to q5
+            elif char =='_':
+                state = 'q5'
+            else:
+                return False
+            
+        elif state == 'q5':
+            ## after underscore, must have digit 0-7, transition back to q4
+            if '0' <= char <= '7':
+                state = 'q4'
+            else:
+                return False
+            
+    ##if we end in state q4, return True
+    if state == 'q4':
         return True
     else:
         return False
